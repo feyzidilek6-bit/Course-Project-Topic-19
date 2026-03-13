22de7448db29ebb1f08b67b4cbfab5fb2e8f8c8b
feyzidilek6-bit <feyzidilek6@gmail.com>
2026-03-13 18:37:50 +0200

    Update ItemService.java
    Миналата седмица направих началото на проекта създадох 3 класа: Main-стартира програмата, Item- описва обект id и name и ItemService - управлява списък с items и ги показва. Тази седмица подобрих ItemService, като добавих меню и въвеждане от клавиатурата, така че потребителят да може :да добавя нов item, да вижда всички items и да излиза от програмата.

--- src/com/project/oop/service/ItemService.java
+++ src/com/project/oop/service/ItemService.java
@@ -2,21 +2,63 @@ package com.project.oop.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import com.project.oop.model.Item;
 
 public class ItemService {
 
     private List<Item> items = new ArrayList<>();
+    private Scanner scanner = new Scanner(System.in);
 
     public void start() {
 
-        System.out.println("System started...");
+        boolean running = true;
 
-        addItem(1, "Example Item 1");
-        addItem(2, "Example Item 2");
+        while (running) {
 
-        showItems();
+            System.out.println("\n=== MENU ===");
+            System.out.println("1. Add item");
+            System.out.println("2. Show items");
+            System.out.println("3. Exit");
 
+            System.out.print("Choose option: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+
+                case 1:
+                    createItem();
+                    break;
+
+                case 2:
+                    showItems();
+                    break;
+
+                case 3:
+                    running = false;
+                    System.out.println("System stopped.");
+                    break;
+
+                default:
+                    System.out.println("Invalid option.");
+            }
+
+        }
+    }
+
+    private void createItem() {
+
+        System.out.print("Enter item id: ");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Enter item name: ");
+        String name = scanner.nextLine();
+
+        addItem(id, name);
+
+        System.out.println("Item added successfully!");
     }
 
     public void addItem(int id, String name) {
@@ -25,6 +67,11 @@ public class ItemService {
     }
 
     public void showItems() {
+
+        if (items.isEmpty()) {
+            System.out.println("No items in system.");
+            return;
+        }
 
         System.out.println("Items in system:");
 
